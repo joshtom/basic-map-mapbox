414ae4adbaa1ee4c0245b128eb84b0460e2d1ae3
joshtom <olajidejoshua4real@gmail.com>
2019-11-12 15:41:23 +0100

    Added all chapters and created a new file

--- scrolltelling/config.js
+++ scrolltelling/config.js
@@ -1,9 +1,9 @@
 var config = {
     style: 'mapbox://styles/joshtom/ck2vsj0xm07nk1clftlk72e03', //TODO
     accessToken: 'pk.eyJ1Ijoiam9zaHRvbSIsImEiOiJjazFranJtN28wYnpiM21wbngyYnVmNWVhIn0._hZgXozsX5twECrg2li7_g', //TODO
-    title: 'YOUR MAP TITLE', //TODO
-    subtitle: 'YOUR MAP SUBTITLE', //TODO
-    byline: 'By YOUR NAME', //TODO
+    title: 'Here is a short story', //TODO
+    subtitle: 'A little about me', //TODO
+    byline: 'Olajide Joshua', //TODO
     footer: 'Created at Concatenate Lagos 2019',
     showMarkers: false,
     theme: 'light',
@@ -16,14 +16,30 @@ var config = {
             description: 'A free conference for African developers with speakers from around the world.',
             location: {
               "center": [
-                3.366639462826271,
-                6.51635441424402
+                3.8976850618724654,
+                7.374839863613502
               ],
-              "zoom": 11.805394255186213,
-              "pitch": 33.499999999999986,
-              "bearing": 136.79999999999995
+              "zoom": 9.359638284520218,
+              "pitch": 0,
+              "bearing": 0
             }
         },
+        {
+            id: 'second-concatenate-lagos',
+            title: 'Second Concatenate Lagos',
+            description: 'Speakers from around the world.',
+            location: {
+              "center": [
+                4.1026452192590455,
+                7.295225165501876
+              ],
+              "zoom": 9.359638284520218,
+              "pitch": 0,
+              "bearing": 0
+            }
+        }
+
+        
         //TODO Copy-paste more chapters here - one for each point in your dataset!
     ]
 };
