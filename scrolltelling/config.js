46778d9c8e40217eebe4cff0666105422bb4b2e0
joshtom <olajidejoshua4real@gmail.com>
2019-11-12 17:15:29 +0100

    Added the first chapter for the article

--- scrolltelling/config.js
+++ scrolltelling/config.js
@@ -1,10 +1,10 @@
 var config = {
     style: 'mapbox://styles/joshtom/ck2vsj0xm07nk1clftlk72e03', //TODO
     accessToken: 'pk.eyJ1Ijoiam9zaHRvbSIsImEiOiJjazFranJtN28wYnpiM21wbngyYnVmNWVhIn0._hZgXozsX5twECrg2li7_g', //TODO
-    title: 'Here is a short story', //TODO
-    subtitle: 'A little about me', //TODO
-    byline: 'Olajide Joshua', //TODO
-    footer: 'Created at Concatenate Lagos 2019',
+    title: 'Events in 2019', //TODO
+    subtitle: 'Listed below are Few Unmissable events in 2019', //TODO
+    byline: 'Olajide Joshua | 11th Nov, 2019', //TODO
+    footer: 'This workshop was provided through the courtesy of Anjana Vakil',
     showMarkers: false,
     theme: 'light',
     alignment: 'left',
@@ -12,8 +12,8 @@ var config = {
         //TODO replace this data with data from the first point in your dataset
         {
             id: 'concatenate-lagos',
-            title: 'Concatenate Lagos',
-            description: 'A free conference for African developers with speakers from around the world.',
+            title: 'DOLOMITI BALLOON FESTIVAL, DOBBIACO, ITALY – 8TH – 17TH JANUARY',
+            description: 'The only thing better than watching dozens of vibrant hot air balloons take flight above the magnificent snow-capped peaks of the Dolomites is, perhaps, being a part of the spectacle. Combine this magical experience with a heart-pumping ski and snowboard vacation in one of the world’s largest ski resorts.',
             location: {
               "center": [
                 3.8976850618724654,
